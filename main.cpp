1e1b75779eda8ac612b32dd265c0beab51c71af8
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-09-23 14:16:33 +0000

    Degisken Tipleri ve Degiskenler

--- main.cpp
+++ main.cpp
@@ -3,9 +3,33 @@
 using namespace std;
 
 int main() {
-  cout << "Merhaba dünya" << endl;
-  cout << "Marhaba dünta 2\n";
-  cout << "Merhaba Dünya 3";
+  //Degisken Tipleri ve degişkenler
+  /*
+    int
+    float
+    double
+    char
+    boll
+  */
+
+  int a = 32;
+  float b = 3.52;
+  double c = 4.546;
+  char d = 'A';
+  bool e = false;
+  
+  cout << "a değişkeninin değeri: " << a << endl;
+  cout << "b değişkeninin değeri: " << b << endl;
+  cout << "c değişkeninin değeri: " << c << endl;
+  cout << "d değişkeninin değeri: " << d << endl;
+  cout << "e değişkeninin değeri: " << e << endl;
+
+  int x,y,z,toplam;
+  x = 4;
+  y = 3;
+  z = 5;
+  toplam = x + y + z;
+  cout << toplam;
   
   return 0;
 }
