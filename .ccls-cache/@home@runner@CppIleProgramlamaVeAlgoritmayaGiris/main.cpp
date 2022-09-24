3d215c7eda577e26a16c1d7e50847cb5776c2a01
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-09-24 12:45:32 +0000

    While Do while ve for döngüsü

--- .ccls-cache/@home@runner@CppIleProgramlamaVeAlgoritmayaGiris/main.cpp
+++ .ccls-cache/@home@runner@CppIleProgramlamaVeAlgoritmayaGiris/main.cpp
@@ -3,22 +3,39 @@
 using namespace std;
 
 int main() {
-  string bys_username = "mustafa_murat";
-  string bys_password = "123456";
-
-  string username, password;
-
-  cout << "Kullanıcı adı ve parolanızı giriniz.\n";
-  cout << "Kullanıcı adı: ";
-  cin >> username;
-  cout << "Parola: ";
-  cin >> password;
-
-  if (bys_username == username && bys_password == password) {
-    cout << "Hoş geldiniz";
-  } else if (bys_username != username && bys_password == password) {
-    cout << "Kullanıcı adı Hatalı şifre doğru";
+  /*
+  int i = 1;
+  int j = 1;
+  while (i < 10 && j < 5) {
+    cout << "i: " << i << "  j: " << j << endl;
+    i += 1;
+    i += 2;
   }
+  */
 
+  /*
+  string parola = "1234";
+  string input;
+
+  do {
+    cout << "Parola gir: ";
+    cin >> input;
+  } while (input != parola);
+
+  cout << "Parola doğru";
+  */
+
+  /*
+  int sayi;
+  cout << "Sayi: ";
+  cin >> sayi;
+
+  int fakt = 1;
+  for (int i = 1; i <= sayi; i++) {
+    fakt *= i;
+  }
+  cout << "Faktoriyel: " << fakt << endl;
+  */
+  
   return 0;
 }
