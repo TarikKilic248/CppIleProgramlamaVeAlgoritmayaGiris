4e9edf3504fd66a72c3ed7f71c0f4dd7bbc453fb
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-09-24 14:23:27 +0000

    Contiune ve break komutları

--- main.cpp
+++ main.cpp
@@ -3,39 +3,28 @@
 using namespace std;
 
 int main() {
+
   /*
-  int i = 1;
-  int j = 1;
-  while (i < 10 && j < 5) {
-    cout << "i: " << i << "  j: " << j << endl;
-    i += 1;
-    i += 2;
+  int i = 0;
+  while (i < 10) {
+    if (i == 5) {
+      break;
+    }
+    cout << "i: " << i << endl;
+    i++;
   }
   */
 
-  /*
-  string parola = "1234";
-  string input;
-
-  do {
-    cout << "Parola gir: ";
-    cin >> input;
-  } while (input != parola);
-
-  cout << "Parola doğru";
-  */
-
-  /*
-  int sayi;
-  cout << "Sayi: ";
-  cin >> sayi;
-
-  int fakt = 1;
-  for (int i = 1; i <= sayi; i++) {
-    fakt *= i;
-  }
-  cout << "Faktoriyel: " << fakt << endl;
-  */
   
+  for (int i = 0; i < 10; i++) {
+    if (i == 3 || i == 5 || i == 7) {
+      continue;
+    }
+    cout << "i: " << i << endl;
+  }
+  
+
+  
+
   return 0;
 }
