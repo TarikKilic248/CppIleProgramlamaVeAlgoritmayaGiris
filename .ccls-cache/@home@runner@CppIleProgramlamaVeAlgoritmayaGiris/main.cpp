9276d4651c3e8f31a54c6a715960f3cd52685d50
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-09-25 15:30:02 +0000

    pointerlar ve arrayler arasındaki ilişki

--- .ccls-cache/@home@runner@CppIleProgramlamaVeAlgoritmayaGiris/main.cpp
+++ .ccls-cache/@home@runner@CppIleProgramlamaVeAlgoritmayaGiris/main.cpp
@@ -2,19 +2,43 @@
 
 using namespace std;
 
+void degerDegistir(int *ptr) {
+  *ptr = 20;
+  cout << "Fonksiyon içindeki deger: " << *ptr << endl;
+}
+
 int main() {
 
-  int a = 5;
-  int *ptr = &a;
+  /*
+  int a = 10;
+  cout << "a degişkeninin degeri: " << a << endl;
+  degerDegistir(&a);
+  cout << "Fonksiyon bittikten sonra a degişkenin degeri: " << a << endl;
+  */
 
-  cout << "a nın adresi: " << &a << endl;
-  cout << "a nın adresi: " << a << endl;
-  cout << "a nın adresi: " << ptr << endl;
-  cout << "Degiskenin degeri: " << *ptr << endl;
+  // int array[] = {1, 2, 3, 4};
+  /*
+  cout << array << endl;
+  cout << array + 1 << endl;
+  cout << array + 2 << endl << endl;
 
-  *ptr = 6; // yıldız pointerin işaret ettiği hücrenin içerisine giriş sağlıyor
-  
-  cout << "Degiskenin yeni degeri: " << a << endl;
-  
+  int *ptr = array;
+  cout << ptr << endl;
+  ptr = ptr + 1;
+  cout << "Pointerın yeni değeri: " << ptr << endl;
+  */
+
+  /*
+  int *ptr = &array[2];
+  cout << ptr << endl;
+  */
+
+  string array[] = {"mustafa", "murat", "coşkun"};
+  string *ptr = array;
+  cout << ptr + 1 << endl;
+  cout << *(ptr + 1) << endl;
+
+  cout << ptr[1] << endl;
+
   return 0;
 }
