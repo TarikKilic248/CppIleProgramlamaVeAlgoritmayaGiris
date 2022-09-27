dd7d4bb88089dc2bccc11cf4c727a3a78853371d
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-09-27 16:21:31 +0000

    arrayle ve fonksiyonlar const anahtar kelimesi pointer ve const structlar pointerlar ve structlar

--- main.cpp
+++ main.cpp
@@ -2,17 +2,47 @@
 
 using namespace std;
 
-void degerDegistir(int &ref) { ref = 20; }
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    cout << array[i] << endl;
+  }
+}
+void printArray2(int *prt, int size) {
+  for (int i = 0; i < size; i++) {
+    cout << prt[i] << endl;
+  }
+}
+void printConstArray(const int *ptr1, const int *ptr2) {
+  for (; ptr1 != ptr2; ptr1++) {
+    cout << "Eleman: " << *ptr1 << endl;
+  }
+}
+struct Employee {
+  int id;
+  string name;
+  string department;
+};
 
 int main() {
+  /*
+  // Arayler ve fonksiyonlar
+  int a[] = {1, 2, 3, 4, 5};
+  printArray(a, 5);
+  printArray2(a, 5);
 
-  int a = 10;
-  // int &ref = a;
-  // ref++;
-  cout << "a'nın değeri: " << a << endl;
+  // Const
+  const int w = 30;
+  const int q[] = {10, 20, 30, 40, 50};
 
-  degerDegistir(a);
-  cout<<"a'nın yeni değeri: "<<a<<endl;
+  // Pointerlar ve const
+  int array[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+  printConstArray(array + 2, array + 7);
+  */
+  // Yapılar(Structurelar)
+  Employee isci1 = {12, "Mustafa", "Bilişim"};
+  cout << isci1.name << endl;
+  isci1.name = "Kazım";
+  cout << isci1.name << endl;
 
   return 0;
 }
