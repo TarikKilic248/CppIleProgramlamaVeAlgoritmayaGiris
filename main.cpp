d67fadd56e7c7cbc93d4280e4e4d34a61cec175b
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-09-28 10:29:24 +0000

    Class oluşturma ve Classdan Obje üretme

--- main.cpp
+++ main.cpp
@@ -2,15 +2,17 @@
 
 using namespace std;
 
-void selamla() { cout << "Merhaba" << endl; }
-void selamla(string name) { cout << "Mehaba" << name << endl; }
-void selamla(string name1,string name2){
-  cout<<"Merhaba"<<name1<<","<<" "<<name2<<endl;
-}
+class Student {
+public:
+  string name;
+};
 
 int main() {
-  selamla();
-  selamla("Mustafa");
-  selamla("Mustafa","Oğuz");
-  return 0;
-}
+  Student student1;
+  Student student2;
+  student1.name="Mustafa Murat";
+  student2.name="Oğuz";
+  cout<<"Öğrenci 1 İsmi: "<<student1.name<<endl;
+  cout<<"Öğrenci 2 İsmi: "<<student2.name<<endl;
+  
+  return 0; }
