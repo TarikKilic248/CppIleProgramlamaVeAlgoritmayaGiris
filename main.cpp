a0c6118df506dbe0901ec7a890644ec35adaaaee
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-09-28 10:33:55 +0000

    Classlara metod ekleme

--- main.cpp
+++ main.cpp
@@ -5,14 +5,16 @@ using namespace std;
 class Student {
 public:
   string name;
+  void tellName() { cout << "Benim adım: " << name << endl; }
 };
 
 int main() {
   Student student1;
   Student student2;
-  student1.name="Mustafa Murat";
-  student2.name="Oğuz";
-  cout<<"Öğrenci 1 İsmi: "<<student1.name<<endl;
-  cout<<"Öğrenci 2 İsmi: "<<student2.name<<endl;
-  
-  return 0; }
+  student1.name = "Mustafa Murat";
+  student2.name = "Oğuz";
+  student1.tellName();
+  student2.tellName();
+
+  return 0;
+}
