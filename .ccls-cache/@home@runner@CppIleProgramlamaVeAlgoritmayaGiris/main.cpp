5bb93419b51efec4898675b70f50c4d60518b585
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-09-28 10:17:56 +0000

    foreach döngüsü

--- .ccls-cache/@home@runner@CppIleProgramlamaVeAlgoritmayaGiris/main.cpp
+++ .ccls-cache/@home@runner@CppIleProgramlamaVeAlgoritmayaGiris/main.cpp
@@ -2,33 +2,12 @@
 
 using namespace std;
 
-struct Employee {
-  int id;
-  string name;
-  string department;
-};
-// FOnksiyon olarak yazdırma
-void showEmployee(Employee employee) {
-  cout << "Id: " << employee.id << endl;
-  cout << "Name: " << employee.name << endl;
-  cout << "Department: " << employee.department << endl;
-}
-// Pointer olarak yazdırma
-void show(Employee *employee) {
-  cout << "Id: " << employee->id << endl;
-  cout << "Name: " << employee->name << endl;
-  cout << "Department: " << employee->department << endl;
-}
-
 int main() {
-  Employee employee1 = {12, "Mustafa Murat", "Bilişim"};
-  Employee *ptr = &employee1;
-  cout << employee1.name << endl;
-  cout << ptr->department << endl;
-
-  showEmployee(employee1);//fonksiyon
-  
-  show(&employee1);//pointer
+  int a[] = {10, 20, 30, 40, 50};
+  //foreach döngüsü
+  for (int item : a) {
+    cout << item << endl;
+  }
 
   return 0;
 }
