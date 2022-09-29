281628503de4cc29ccaa1bb49c1f6e8de8becd77
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-09-29 07:44:28 +0000

    pointerlar ve classlar

--- main.cpp
+++ main.cpp
@@ -4,16 +4,28 @@ using namespace std;
 class Employee {
 private:
   string name;
+  int age;
 
 public:
   void setName(string isim) { name = isim; }
   string getName() { return name; }
+  void setAge(int yas) {
+    if (yas < 0) {
+      cout << "0 < x olmalıdır!" << endl;
+    } else {
+      age = yas;
+    }
+  }
+  int getAge() { return age; }
 };
 
 int main() {
-  Employee employee;
-  employee.setName("Mustafa Murat");
-  cout << employee.getName() << endl;
+  Employee *employee = new Employee();
+  employee->setName("Mustafa Murat");
+  employee->setAge(25);
+
+  cout << employee->getName()<<endl;
+  cout << employee->getAge()<<endl;
 
   return 0;
 }
