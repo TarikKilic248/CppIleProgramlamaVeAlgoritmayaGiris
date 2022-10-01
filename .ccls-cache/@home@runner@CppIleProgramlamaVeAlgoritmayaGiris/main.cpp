9fd773520a527e52541d82177453a08eaa957c92
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-10-01 10:27:56 +0000

    Destructor oluşturuldu

--- .ccls-cache/@home@runner@CppIleProgramlamaVeAlgoritmayaGiris/main.cpp
+++ .ccls-cache/@home@runner@CppIleProgramlamaVeAlgoritmayaGiris/main.cpp
@@ -2,46 +2,14 @@
 using namespace std;
 
 class Employee {
-private:
-  string name;
-  int salary;
-  int id;
-
 public:
-  // constracter
-  Employee() {
-    this->name = "Bilgi yok";
-    this->salary = 0;
-    this->id = 0;
-  }
-  Employee(string name, int salary) {
-    this->name = name;
-    this->salary = salary;
-    this->id = 0;
-  }
-  Employee(string name, int salary, int id) {
-    this->name = name;
-    this->salary = salary;
-    this->id = id;
-  }
-  void setSalary(int salary) { this->salary = salary; }
-  int getSalary() { return salary; }
-
-  void setName(string name) { this->name = name; }
-  string getName() { return name; }
-  void showInfos() {
-    cout << "İsim: " << this->name << endl;
-    cout << "Maaş: " << this->salary << endl;
-    cout << "Id: " << this->id << endl;
-  }
+  Employee() { cout << "Constuctor çağırıldı." << endl; }
+  ~Employee() { cout << "Destructor çağırıldı" << endl; }
 };
 
 int main() {
-  Employee employee1("Mustafa Murat", 3000, 12);
-  Employee employee2("Oğuz", 400);
-  Employee employee3;
-  employee1.showInfos();
-  employee2.showInfos();
-  employee3.showInfos();
+  Employee *emp = new Employee(); // obje oluşturuldu
+  delete emp;
+
   return 0;
 }
