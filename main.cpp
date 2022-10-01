2ecd1296d16e5de9f9c1376558e8da43bf2280ac
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-10-01 11:15:53 +0000

    Destructor ve bellek sızıntıları

--- main.cpp
+++ main.cpp
@@ -3,12 +3,27 @@ using namespace std;
 
 class Employee {
 public:
-  Employee() { cout << "Constuctor çağırıldı." << endl; }
-  ~Employee() { cout << "Destructor çağırıldı" << endl; }
+  string *s;
+  int *i;
+  Employee(string str, int ivalue) {
+    s = new string;
+    i = new int;
+    *s = str;
+    *i = ivalue;
+  }
+  void show() {
+    cout << "String değer: " << *s << endl << "Int değer:" << *i << endl;
+  }
+  ~Employee() {
+    cout << "Destructor çağırıldı" << endl;
+    delete s;
+    delete i;
+  }
 };
 
 int main() {
-  Employee *emp = new Employee(); // obje oluşturuldu
+  Employee *emp = new Employee("Mustafa", 25);
+  emp->show();
   delete emp;
 
   return 0;
