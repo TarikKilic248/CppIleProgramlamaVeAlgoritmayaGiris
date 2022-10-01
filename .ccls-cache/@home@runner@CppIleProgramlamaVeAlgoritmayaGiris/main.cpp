f8a652b2b99861c064f55547bbd357266e6139ae
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-10-01 11:24:20 +0000

    static değişkenler

--- .ccls-cache/@home@runner@CppIleProgramlamaVeAlgoritmayaGiris/main.cpp
+++ .ccls-cache/@home@runner@CppIleProgramlamaVeAlgoritmayaGiris/main.cpp
@@ -1,30 +1,13 @@
 #include <iostream>
 using namespace std;
 
-class Employee {
-public:
-  string *s;
-  int *i;
-  Employee(string str, int ivalue) {
-    s = new string;
-    i = new int;
-    *s = str;
-    *i = ivalue;
-  }
-  void show() {
-    cout << "String değer: " << *s << endl << "Int değer:" << *i << endl;
-  }
-  ~Employee() {
-    cout << "Destructor çağırıldı" << endl;
-    delete s;
-    delete i;
-  }
-};
+void Test() {
+  static int i = 3;
+  i++;
+  cout << "i'nin değeri: " << i << endl;
+}
 
 int main() {
-  Employee *emp = new Employee("Mustafa", 25);
-  emp->show();
-  delete emp;
-
+  Test();
   return 0;
 }
